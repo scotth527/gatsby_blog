ca7bf6564890f2450b2e31fb3dbc416fc8a7cceb
Scott Huang <scottyh527@gmail.com>
2019-05-17 19:53:57 +0000

    added fort awesome and social media icons to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,11 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import MyNavbar from "./navbar.js"
 import "./layout.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,9 +31,9 @@ const Layout = ({ children }) => (
           <main>{children}</main>
           <footer className="text-center pb-2 pt-2">
             Follow me on 
-            <a className="ml-1 mr-1" target="_blank" href="https://github.com/scotth527">Github</a>
-            <a className="ml-1 mr-1" target="_blank" href="https://www.linkedin.com/in/scotth527/">Linkedin</a>
-            <a className="ml-1 mr-1" target="_blank" href="https://stackoverflow.com/users/11364096/scotth527?tab=profile">Stack Overflow</a>
+            <a className="ml-1 mr-1 text-dark" target="_blank" href="https://github.com/scotth527"><FontAwesomeIcon size="2x" icon={faGithub} /></a>
+            <a className="ml-1 mr-1 text-dark" target="_blank" href="https://www.linkedin.com/in/scotth527/"><FontAwesomeIcon size="2x" icon={faLinkedin} /></a>
+            <a className="ml-1 mr-1 text-dark" target="_blank" href="https://stackoverflow.com/users/11364096/scotth527?tab=profile"><FontAwesomeIcon size="2x" icon={faStackOverflow} /></a>
           </footer>
        
       </div>
