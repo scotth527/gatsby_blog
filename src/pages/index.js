2632618e16a0534e55f7c678ccd61ab407d94254
Scott Huang <scottyh527@gmail.com>
2019-05-19 01:48:09 +0000

    added a banner with a google font

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,12 @@ export class IndexPage extends React.Component {
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <div className="container-fluid wrapper">
           <div className="row">
-            <div style={{height:"400px", backgroundSize: "cover contain",  backgroundRepeat: "no-repeat", backgroundImage: `url(${'https://images.pexels.com/photos/792199/pexels-photo-792199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'})`}} className="col-12 text-dark text-center mx-auto">
-              <h1>Welcome to my site! </h1>
+            <div style={{height:"400px", backgroundSize: "cover", backgroundPosition: 'center', backgroundRepeat: "no-repeat", backgroundImage: `url(${'https://images.pexels.com/photos/792199/pexels-photo-792199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'})`}} className="col-12 text-dark text-center text-align center mx-auto">
+              <h1 className="banner-text mt-5">Welcome to my site! </h1>
+              <h2 className="banner-text mt-5">I'm a Junior developer looking to build cool things</h2>
             </div>
           </div>
-          <div className="text-center mx-auto mt-3 mb-3 d-flex justify-content-around">
+          <div className="text-center mx-auto mt-3 mb-3 d-flex justify-content-center">
             <div>
               <h1 className="mt-3">Hi people</h1>
               <p>Welcome to my page.</p>
